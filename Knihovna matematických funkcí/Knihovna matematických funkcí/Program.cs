46f3ce68e4f31616e5b1c4c95ae2b27c74bf98c1
Tomius5 <bor.tom555@gmail.com>
2025-10-20 09:43:26 +0200

    ahoj
    dnešek je LLL

---
+++ Knihovna matematických funkcí/Knihovna matematických funkcí/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knihovna_matematických_funkcí
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Mathfunction
+    {
+        public static string Name { get; }
+        public static string Description { get; }
+        public Interval Domain { get; set; } 
+        public Interval Range { get; }
+    }
+    public struct Interval 
+    {
+        public double down;
+        public double up;
+        public // Vyřešit typ závorek.
+    }
+}
