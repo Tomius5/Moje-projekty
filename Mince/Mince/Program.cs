06c1e433e822d17364ba54b5add0f91faad66fe4
Tomius5 <bor.tom555@gmail.com>
2026-03-27 08:46:33 +0100

    dú
    Snad poslední

---
+++ Mince/Mince/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mince
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        static void Run()
+        {
+            Console.WriteLine("Zadej mince:");
+
+            // List<int> mince = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            List<int> mince = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Console.WriteLine("Zadej částku:");
+            int cil = int.Parse(Console.ReadLine());
+            List<List<int>> vysledky = new List<List<int>>();
+
+
+
+            Rekurze(cil, mince, new List<int>(), 0, vysledky);
+
+
+
+            if (vysledky.Count == 0)
+            {
+                Console.WriteLine("Žádné řešení");
+            }
+            else
+            {
+
+                foreach (var v in vysledky)
+                {
+                    Console.WriteLine(string.Join(" ", v));
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+
+
+        static void Rekurze(int cil, List<int> mince, List<int> current, int od, List<List<int>> vysledky)
+        {
+
+
+            //int soucet = 0;
+            //foreach(var x in current)
+            //{
+            //    soucet += x;
+            //}
+
+
+            int soucet = current.Sum();
+
+            if (soucet == cil)
+            {
+                vysledky.Add(new List<int>(current));
+                return;
+            }
+
+
+
+            for (int i = od; i < mince.Count; i++)
+            {
+                if (soucet + mince[i] <= cil)
+                {
+                    current.Add(mince[i]);
+                    Rekurze(cil, mince, current, i, vysledky);
+                    current.RemoveAt(current.Count - 1);
+                }
+
+
+                // Console.WriteLine(mince[i]);
+            }
+        }
+
+
+
+    }
+}
---
+++ Posloupnosti/Posloupnosti/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Posloupnosti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej posloupnost:");
+            List<int> x = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int n = x.Count;
+            // x.Add(int.MinValue);
+            x.Insert(0, int.MinValue);
+            int[] T = new int[n + 1];
+            int[] P = new int[n + 1];
+
+
+
+            for (int i = n; i >= 0; i--)
+            {
+                T[i] = 1;
+                P[i] = 0;
+
+                for (int j = i + 1; j <= n; j++)
+                {
+                    if (x[i] < x[j] && T[i] < 1 + T[j])
+                    {
+                        T[i] = 1 + T[j];
+                        P[i] = j;
+                    }
+
+                }
+
+
+            }
+            // Console.WriteLine(T[0]);
+            Console.WriteLine("Délka nejdelší posloupnosti: " + (T[0] - 1));
+            List<int> vysledek = new List<int>();
+            int k = P[0];
+            while (k != 0)
+            {
+                vysledek.Add(x[k]);
+
+                k = P[k];
+            }
+            Console.WriteLine("Podposloupnost:");
+            // Console.WriteLine(vysledek);
+            Console.WriteLine(string.Join(" ", vysledek));
+            Console.ReadLine();
+        }
+
+    }
+}
